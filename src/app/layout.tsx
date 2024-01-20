0ac091711033b20867a61376a19d8a232eaf45bd
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-20 15:20:06 -0600

    setting up shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/shared/components/theme-provider";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -19,7 +20,17 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={`${openSans.className} bg-background dark:bg-backgroundBlack text-foreground dark:text-foregroundBlack`}>{children}</body>
+      <body className={`${openSans.className}`}>
+        <ThemeProvider
+          attribute="class"
+          themes={["light", "dark"]}
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@shared/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>OpenTasks</h1>
+      <Button>Hello</Button>
     </main>
   );
 }
